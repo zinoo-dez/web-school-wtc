91b80fb2f7679dc4c63554e2c414525fb42e09cc
zinoo-dez <zinoo.dev@gmail.com>
2025-03-04 00:03:25 +0630

    add menu

--- src/app/components/Background-Beam.tsx
+++ src/app/components/Background-Beam.tsx
@@ -7,27 +7,31 @@ export default function BackgroundBeamsDemo() {
     return (
         <>
 
-            <div className="min-h-[90vh] w-full rounded-md bg-neutral-950 dark:bg-neutral-900 relative flex items-center justify-center flex-col  antialiased">
+            <div className="min-h-[90vh] w-full rounded-md bg-neutral-950 dark:bg-neutral-900   antialiased">
                 <Nav />
-                <Image
-                    src="/gg.png"
-                    alt="coding"
-                    width={500}
-                    height={500}
-                    className="hidden md:block object-cover absolute right-[0px] top-[100px]"
-                />
-                <div className="max-w-2xl mx-auto p-4">
-                    <h1 className="relative z-10 text-3xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-                        <span className="text-yellow-500">Join</span> <span className="text-green-500">the</span> <span className="text-yellow-500">WTC</span><br />
-                        <span className="bg-clip-text text-transparent bg-gradient-to-b from-yellow-500 to-green-500">Software Development Club</span>
-                    </h1>
-                    <p></p>
-                    <p className="text-neutral-300 max-w-lg mx-auto my-2 text-md text-center relative z-10">
-                        Learn web development with us. <br />
-                        We offer Web Foundation, JavaScript Developer, and PHP Developer classes. <br />
-                        Contact us to learn more!
-                    </p>
+                <div className="min-h-[80vh] relative flex items-center justify-center flex-col">
 
+
+                    <Image
+                        src="/gg.png"
+                        alt="coding"
+                        width={500}
+                        height={500}
+                        className="hidden md:block object-cover absolute right-[0px] top-[100px]"
+                    />
+                    <div className="max-w-2xl mx-auto p-4">
+                        <h1 className="relative z-10 text-3xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+                            <span className="text-yellow-500">Join</span> <span className="text-green-500">the</span> <span className="text-yellow-500">WTC</span><br />
+                            <span className="bg-clip-text text-transparent bg-gradient-to-b from-yellow-500 to-green-500">Software Development Club</span>
+                        </h1>
+                        <p></p>
+                        <p className="text-neutral-300 max-w-lg mx-auto my-2 text-md text-center relative z-10">
+                            Learn web development with us. <br />
+                            We offer Web Foundation, JavaScript Developer, and PHP Developer classes. <br />
+                            Contact us to learn more!
+                        </p>
+
+                    </div>
                 </div>
                 <BackgroundBeams />
             </div>
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,16 +2,34 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Nav() {
+    const [isOpen, setIsOpen] = React.useState(false)
     return (
-        <div className='h-[90px] px-10 flex justify-between items-center '>
+        <div className='h-[90px] px-10 relative flex justify-between items-center z-[1000]'>
             <Link href="/"><span className='font-bold text-3xl text-yellow-500'>W</span><span className='font-bold text-xl text-green-500'>T</span><span className='font-bold text-xl text-red-500'>C</span></Link>
-            <ul className='flex gap-4'>
+            <ul className='hidden md:flex gap-4'>
                 <Link className='text-lg text-slate-100 hover:shadow-amber-100' href="/">Home</Link>
                 <Link className='text-lg text-slate-100 hover:shadow-amber-100' href="#about">About</Link>
                 <Link className='text-lg text-slate-100 hover:shadow-amber-100' href="#contact">Class</Link>
                 <Link className='text-lg text-slate-100 hover:shadow-amber-100' href="#blog">Blog</Link>
                 <Link className='text-lg text-slate-100 hover:shadow-amber-100' href="#contact">Contact</Link>
             </ul>
+            <button
+                className='md:hidden flex flex-col gap-2 cursor-pointer'
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <span className='w-8 h-0.5 bg-slate-100' />
+                <span className='w-8 h-0.5 bg-slate-100' />
+                <span className='w-8 h-0.5 bg-slate-100' />
+            </button>
+            {isOpen && (
+                <ul className='md:hidden absolute top-20 w-full m-auto right-0 bg-[#0f172a88] backdrop-blur-md rounded-lg shadow-lg z-50 p-10'>
+                    <Link className='text-lg text-slate-100 block text-center hover:text-slate-500 mb-3' href="/">Home</Link>
+                    <Link className='text-lg text-slate-100 block text-center hover:text-slate-500 mb-3' href="#about">About</Link>
+                    <Link className='text-lg text-slate-100 block text-center hover:text-slate-500 mb-3' href="#contact">Class</Link>
+                    <Link className='text-lg text-slate-100 block text-center hover:text-slate-500 mb-3' href="#blog">Blog</Link>
+                    <Link className='text-lg text-slate-100 block text-center hover:text-slate-500' href="#contact">Contact</Link>
+                </ul>
+            )}
         </div>
     )
 }
