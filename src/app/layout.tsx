b080b7af68c4e795bfbebc5849bdbc3284799ab7
zinoo-dez <zinoo.dev@gmail.com>
2025-03-03 14:34:39 +0630

    fix code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Philosopher } from "next/font/google";
 import "./globals.css";
-import Nav from "./components/Nav";
 
 const philosopher = Philosopher({
   subsets: ['latin'],
   weight: ['400', '700'], // Match your weights
   style: ['normal', 'italic'], // Include italic
   variable: '--font-philosopher', // Optional: CSS variable
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
@@ -35,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${philosopher.variable} ${geistMono.variable} antialiased`}
       >
-
         {children}
       </body>
     </html>
